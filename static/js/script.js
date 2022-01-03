7ef8c38a5b2f414bdadccc53c363d7be85887244
deanoandrejevic <deanoandrejevic@gmail.com>
2022-01-03 23:14:37 +0000

    changed the edit-gig form again as feature that was implemented was not needed

--- static/js/script.js
+++ static/js/script.js
@@ -61,27 +61,13 @@ $(document).ready(function () {
 });
 
 
-// This function is to display different options when a certain category is selected. CREDIT: https://jsfiddle.net/uftr0qa4/2/
+// This function is to display different options when a certain category is selected
 
-var categoryEdit = document.querySelector("#category_name");
-var attendedTargetEdit = document.getElementById("attendedSelectedEdit");
-var upcomingTargetEdit = document.getElementById("upcomingSelectedEdit");
+const category = document.querySelector("#category_name");
+const attendedTargetAdd = document.getElementById("attendedSelectedAdd");
+const upcomingTargetAdd = document.getElementById("upcomingSelectedAdd");
 
-category.addEventListener("change", function () {
-    if (categoryEdit.value == "Attended") {
-        attendedTargetEdit.style = "display: block;";
-        upcomingTargetEdit.style = "display: none;";
-    } else if (categoryEdit.value == "Upcoming") {
-        upcomingTargetEdit.style = "display: block;";
-        attendedTargetEdit.style = "display: none;";
-    }
-})
-
-var categoryAdd = document.querySelector("#category_name");
-var attendedTargetAdd = document.getElementById("attendedSelectedAdd");
-var upcomingTargetAdd = document.getElementById("upcomingSelectedAdd");
-
-category.addEventListener("change", function () {
+category.addEventListener("change", "load", function () {
     if (category.value == "Attended") {
         attendedTargetAdd.style = "display: block;";
         upcomingTargetAdd.style = "display: none;";
